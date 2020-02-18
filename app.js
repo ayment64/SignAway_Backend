c3d9d1fcff2cb3dd1b47537b85ebbf0e95ee4675
arifmedamine <mohamedamine.arif@esprit.tn>
2020-02-18 17:11:12 +0100

    user Api done

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+//Init Middelware
+app.use(express.json({ extended: false }))
 
 app.get('/', (req, res)=>{
     res.send('APT running');
 })
+
+// Define routes
+app.use('/api/users', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000 ;
 
